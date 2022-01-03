99e0848daf3b5400c4145288b93f09b020368235
start-27 <minw7092@naver.com>
2022-01-04 00:24:55 +0900

    Feat : 외부 api 호출하여 도서 데이터 받아오는 기능 추가  - 알라딘 api 베스트셀러 리스트  - 알라딘 api 도서 상세 조회  - 알라딘 api 키워드로 검색  - 알라딘 api 카테고리별 조회  - 도서관 정보나루 api 해당 도서에 대한 추천도서 조회

--- src/main/java/com/bookpie/shop/service/BookService.java
+++ src/main/java/com/bookpie/shop/service/BookService.java
@@ -7,18 +7,19 @@ import com.bookpie.shop.domain.dto.book.BookDto;
 import com.bookpie.shop.repository.BookCategoryRepository;
 import com.bookpie.shop.repository.BookRepository;
 import lombok.extern.slf4j.Slf4j;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.io.*;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLEncoder;
-import java.nio.charset.StandardCharsets;
+import java.net.*;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -72,18 +73,128 @@ public class BookService {
                 .collect(Collectors.toList());
     }
 
-    // 도서 추가
+    // 도서 추가 (임시용)
     public BookDto create(BookDto dto) {
         Book book = Book.createBook(dto);
         Book book1 = bookRepository.save(book);
         return BookDto.createDto(book1);
     }
 
+    // 도서 삭제 (임시용)
     public String delete(Long book_id) {
         Book book = bookRepository.findById(book_id).
                 orElseThrow(() -> new IllegalArgumentException("해당 도서는 존재하지 않습니다."));
         bookRepository.delete(book);
         return book.getTitle() + " 도서가 삭제되었습니다.";
     }
+
+    // 알라딘 api 베스트 셀러 조회
+    public JSONObject best(String page, String size) {
+        String ttbKey = "ttbminw70921526001";
+        int realPage = 1;
+        int realSize = 8;
+        if (page != null) realPage = Integer.parseInt(page);
+        if (size != null) realSize = Integer.parseInt(size);
+
+        String uri = "http://www.aladin.co.kr/ttb/api/ItemList.aspx?ttbkey="+ttbKey+"&QueryType=Bestseller" +
+                "&MaxResults="+realSize+"&start="+realPage+"&SearchTarget=Book&output=js&Version=20131101";
+
+        return callApi(uri);
+    }
+
+    // 도서 상세 조회
+    public JSONObject bookDetail(Long isbn) {
+        String ttbKey = "ttbminw70921526001";
+        String uri = "http://www.aladin.co.kr/ttb/api/ItemLookUp.aspx?ttbkey="+ttbKey+"&itemIdType=ISBN13&ItemId="+isbn+"" +
+                "&output=js&Version=20131101&OptResult=authors,reviewList,fulldescription";
+
+        return callApi(uri);
+    }
+
+    // 키워드로 검색색
+    public JSONObject searchKeyword(String queryType, String keyword, String page, String size) {
+        int realPage = 1;
+        int realSize = 16;
+        if (page != null) realPage = Integer.parseInt(page);
+        if (size != null) realSize = Integer.parseInt(size);
+
+        String ttbKey = "ttbminw70921526001";
+        String uri = "http://www.aladin.co.kr/ttb/api/ItemSearch.aspx?ttbkey="+ttbKey+"&Query="+keyword+"&QueryType="+queryType+"" +
+                "&MaxResults="+realSize+"&start="+realPage+"&SearchTarget=Book&output=js&Version=20131101";
+
+        return callApi(uri);
+    }
+
+    // 카테고리로 조회
+    public JSONObject byCategory(Long category_id, String page, String size) {
+        int realPage = 1;
+        int realSize = 16;
+        if (page != null) realPage = Integer.parseInt(page);
+        if (size != null) realSize = Integer.parseInt(size);
+
+        String ttbKey = "ttbminw70921526001";
+        String uri = "http://www.aladin.co.kr/ttb/api/ItemList.aspx?ttbkey="+ttbKey+"&QueryType=ItemEditorChoice" +
+                "&MaxResults="+realSize+"&start="+realPage+"&SearchTarget=Book&CategoryId="+category_id+"&output=js&Version=20131101";
+
+        return callApi(uri);
+    }
+
+    // 추천 도서
+    public JSONArray recommend(Long isbn) {
+        String apiKey = "fea604846208253ec4d2da7945989a95356f50d74b131dc8b1eab0ff34011536";
+        String uri = "http://data4library.kr/api/recommandList?authKey="+apiKey+"&isbn13="+isbn+"&format=json";
+        JSONObject object = null;
+        JSONArray jsonArray = new JSONArray();
+        String line = null;
+
+        try {
+            URL url = new URL(uri);
+            URLConnection conn = url.openConnection();
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
+            line = br.readLine();
+
+            JSONParser jsonParser = new JSONParser();
+            object = (JSONObject) jsonParser.parse(line);
+
+            // 받은 json데이터에서 response 객체를 꺼내고 docs List 생성
+            JSONObject response = (JSONObject) object.get("response");
+            JSONArray docs = (JSONArray) response.get("docs");
+
+            // 기본 값으로 200개의 데이터를 줌. 10개만 넘길거임
+            for (int i = 0; i < 10; i++) {
+                jsonArray.add(docs.get(i));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return jsonArray;
+    }
+
+    // api 호출 메서드
+    private JSONObject callApi(String uri) {
+        JSONObject object = null;
+        String line = null;
+
+        try {
+            URL url = new URL(uri);
+            URLConnection conn = url.openConnection();
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
+            line = br.readLine();
+
+            JSONParser jsonParser = new JSONParser();
+            object = (JSONObject) jsonParser.parse(line);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        return object;
+    }
+
 }
 
