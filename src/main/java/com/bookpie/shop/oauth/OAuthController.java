1d72cbead41f40d851bda1b71bfef8de8ecaa229
kky0426 <ksakdlf99@naver.com>
2022-01-05 17:54:52 +0900

    Feat : 네이버 로그인 기능 추가

--- src/main/java/com/bookpie/shop/oauth/OAuthController.java
+++ src/main/java/com/bookpie/shop/oauth/OAuthController.java
@@ -1,20 +1,13 @@
 package com.bookpie.shop.oauth;
 
-import com.bookpie.shop.utils.ApiUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import net.minidev.json.JSONObject;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.*;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.client.RestTemplate;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.net.URI;
-import java.util.Map;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import static com.bookpie.shop.utils.ApiUtil.success;
 
@@ -25,86 +18,19 @@ import static com.bookpie.shop.utils.ApiUtil.success;
 public class OAuthController {
 
     private final OAuthService oAuthService;
-    @Value("${kakao.client-id}")
-    private String clientId;
 
-    @Value("${kakao.redirect-url}")
-    private String redirectUrl;
-
-    @Value("${kakao.login}")
-    private String loginUrl;
-
-    @Value("${kakao.profile}")
-    private String profileUrl;
-
-    @Value("${kakao.token}")
-    private String tokenUrl;
-
-    @Value("${path.base}")
-    private String baseUrl;
-
-    @GetMapping("/login/kakao")
-    public ResponseEntity kakaoRedirect() throws Exception{
-        String reqUrl = loginUrl+"?client_id="+clientId+"&redirect_uri="+baseUrl+redirectUrl+"&response_type=code";
-        return new ResponseEntity(success(reqUrl),HttpStatus.OK);
-    }
-
-    @GetMapping("/loginresult/kakao")
-    public ResponseEntity kakaoLogin(HttpServletRequest request,HttpServletResponse response) throws Exception{
-        String code = request.getParameter("code");
-        String error = request.getParameter("error");
-        if (error!= null){
-            throw new Exception();
-        }
-        String accessToken = getAccessToken(code);
-        JSONObject profile = getKakaoProfile(accessToken);
-        return new ResponseEntity(success(oAuthService.kakaoLogin(profile)),HttpStatus.OK);
-    }
 
     @GetMapping("/login/kakao/test/{accessToken}")
     public ResponseEntity kakaoLoginTest(@PathVariable("accessToken")String accessToken){
-        JSONObject profile = getKakaoProfile(accessToken);
-        return new ResponseEntity(success(oAuthService.kakaoLogin(profile)),HttpStatus.OK);
+        return new ResponseEntity(success(oAuthService.kakaoLogin(accessToken)),HttpStatus.OK);
+    }
+
+    @GetMapping("/login/naver/{accessToken}")
+    public ResponseEntity naverLogin(@PathVariable("accessToken")String accessToken){
+        return new ResponseEntity(success(oAuthService.naverLogin(accessToken)),HttpStatus.OK);
     }
 
 
-    public String getAccessToken(String code) throws Exception{
-        String accessToken = "";
-
-        RestTemplate restTemplate = new RestTemplate();
-        HttpHeaders headers = new HttpHeaders();
-        MultiValueMap<String,Object> parameters = new LinkedMultiValueMap<>();
-        parameters.set("grant_type","authorization_code");
-        parameters.set("client_id",clientId);
-        parameters.set("redirect_uri",baseUrl+redirectUrl);
-        parameters.set("code",code);
-        HttpEntity<MultiValueMap<String,Object>> request = new HttpEntity<>(parameters,headers);
-        ResponseEntity<JSONObject> response = restTemplate.postForEntity(tokenUrl,request,JSONObject.class);
-        accessToken = (String) response.getBody().get("access_token");
-        log.debug(response.toString());
-        return accessToken;
-    }
-
-    public JSONObject getKakaoProfile(String token){
-        String id = "";
-        RestTemplate restTemplate = new RestTemplate();
-
-        HttpHeaders headers = new HttpHeaders();
-        headers.set("Authorization","bearer "+token);
-        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
-        //headers.set("KakaoAk",clientId);
-
-        MultiValueMap<String,Object> parameters = new LinkedMultiValueMap<>();
-        //parameters.add("property_keys", "[\"id\"]");
-        HttpEntity<MultiValueMap<String ,Object>> request = new HttpEntity(parameters,headers);
-        try {
-            ResponseEntity<JSONObject> response = restTemplate.postForEntity(profileUrl, request, JSONObject.class);
-            return response.getBody();
-        }catch (Exception e){
-            throw new IllegalArgumentException("카카오 사용자 정보를 불러올 수 없습니다.");
-        }
-
-    }
 
 
 }
--- src/main/java/com/bookpie/shop/oauth/OAuthService.java
+++ src/main/java/com/bookpie/shop/oauth/OAuthService.java
@@ -4,50 +4,112 @@ import com.bookpie.shop.config.JwtTokenProvider;
 import com.bookpie.shop.domain.User;
 import com.bookpie.shop.domain.enums.LoginType;
 import com.bookpie.shop.repository.UserRepository;
-import com.bookpie.shop.service.UserSevice;
-import com.google.gson.Gson;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import net.minidev.json.JSONObject;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
-import javax.validation.Valid;
 import java.util.Map;
 import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class OAuthService {
     private final UserRepository userRepository;
     private final JwtTokenProvider jwtTokenProvider;
-    private final UserSevice userSevice;
-    @Transactional
-    public String kakaoLogin(JSONObject profile){
+
+    @Value("${naver.url}")
+    private String naverUrl;
+
+    @Value("${naver.client-id}")
+    private String naverClientId;
+
+    @Value("${naver.client-secret}")
+    private String naverClientSecret;
+
+    @Value("${kakao.profile}")
+
+    private String profileUrl;
+    public String kakaoLogin(String token){
+        JSONObject profile = getKakaoProfile(token);
         Map<String,Object> kakaoAccount = (Map<String, Object>) profile.get("kakao_account");
         Map<String,Object> kakaoProfile = (Map<String, Object>) kakaoAccount.get("profile");
         String name = (String) kakaoProfile.get("nickname");
         String email = (String) kakaoAccount.get("email");
-        Optional<User> optionalUser= userRepository.findByEmail(email);
-        User user;
-        if (optionalUser.isEmpty()){
-            User nUser = User.oauthCreate(email,name, LoginType.KAKAO);
-            
-            if (!userSevice.emailValidation(email)) throw new IllegalArgumentException("이미 가입된 이메일 입니다.");
-
-            Long id = userRepository.save(nUser);
-            user = userRepository.findById(id).orElseThrow(()->new UsernameNotFoundException("사용자를 찾을 수 없습니다."));
-        }else{
-            user = optionalUser.get();
-            if (user.getLoginType() != LoginType.KAKAO){
-                throw new UsernameNotFoundException("로그인 형식이 잘못되었습니다.");
-            }
-        }
+        User user = oauthSaveAndGet(email,name,LoginType.KAKAO);
         return jwtTokenProvider.createToken(user.getUsername(),user.getRoles());
+    }
+
+    public String naverLogin(String token){
+        JSONObject naverProfile = getNaverProfile(token);
+        Map<String,Object> response =(Map<String, Object>) naverProfile.get("response");
+        String email = (String) response.get("email");
+        String name = (String) response.get("nickname");
+        User user = oauthSaveAndGet(email,name,LoginType.NAVER);
+        return jwtTokenProvider.createToken(user.getEmail(),user.getRoles());
+    }
+
+    @Transactional
+    public User oauthSaveAndGet(String eamil,String name,LoginType type){
+        User user;
+        Optional<User> optionalUser = userRepository.findByEmail(eamil);
+        if(optionalUser.isPresent()){
+            user = optionalUser.get();
+            if(user.getLoginType()!= type) throw new IllegalArgumentException("로그인 형식이 잘못되었습니다.");
+        }else{
+            User nUser = User.oauthCreate(eamil,name,type);
+            Long id = userRepository.save(nUser);
+            user = userRepository.findById(id).get();
+        }
+        return user;
+    }
+
+
+
+    public JSONObject getNaverProfile(String token){
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authrization","Bearer "+token);
+        headers.set("X-Naver-Client-Id",naverClientId);
+        headers.set("X-Naver-Client-Secret",naverClientSecret);
+        HttpEntity<MultiValueMap<String,Object>> request = new HttpEntity<>(null,headers);
+        try{
+            ResponseEntity<JSONObject> response = restTemplate.exchange(naverUrl,HttpMethod.GET,request,JSONObject.class);
+            return response.getBody();
+        }catch (Exception e){
+            throw new IllegalArgumentException("네이버 사용자 정보를 불러올 수 없습니다.");
+        }
+    }
+
+    public JSONObject getKakaoProfile(String token){
+        String id = "";
+        RestTemplate restTemplate = new RestTemplate();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization","bearer "+token);
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+        //headers.set("KakaoAk",clientId);
+
+        MultiValueMap<String,Object> parameters = new LinkedMultiValueMap<>();
+        //parameters.add("property_keys", "[\"id\"]");
+        HttpEntity<MultiValueMap<String ,Object>> request = new HttpEntity(parameters,headers);
+        try {
+            ResponseEntity<JSONObject> response = restTemplate.postForEntity(profileUrl, request, JSONObject.class);
+            log.debug(response.toString());
+            return response.getBody();
+        }catch (Exception e){
+            e.printStackTrace();
+            log.debug(e.getMessage());
+            throw new IllegalArgumentException("카카오 사용자 정보를 불러올 수 없습니다.");
+
+        }
+
     }
 }
