392cb365a1511755751b1e0040717696605f98e2
kky0426 <ksakdlf99@naver.com>
2022-01-09 18:19:36 +0900

    Test : 주문 상세내역 조회기능 테스트

--- src/test/java/com/bookpie/shop/repository/OrderRepositoryTest.java
+++ src/test/java/com/bookpie/shop/repository/OrderRepositoryTest.java
@@ -1,6 +1,7 @@
 package com.bookpie.shop.repository;
 
 import com.bookpie.shop.domain.Order;
+import com.bookpie.shop.domain.Point;
 import com.bookpie.shop.domain.UsedBook;
 import com.bookpie.shop.domain.User;
 import com.bookpie.shop.domain.enums.BookState;
@@ -8,12 +9,17 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @ExtendWith(SpringExtension.class)
+@Transactional
 class OrderRepositoryTest {
     @Autowired UserRepository userRepository;
     @Autowired UsedBookRepository usedBookRepository;
@@ -23,6 +29,7 @@ class OrderRepositoryTest {
         return User.builder()
                 .name("user1")
                 .nickName("nick1")
+                .point(Point.createDefaultPoint())
                 .email("user1@gmail.com")
                 .build();
     }
@@ -32,6 +39,7 @@ class OrderRepositoryTest {
                 .name("user2")
                 .nickName("nick2")
                 .email("user2@gmail.com")
+                .point(Point.createDefaultPoint())
                 .build();
     }
 
@@ -45,7 +53,6 @@ class OrderRepositoryTest {
     }
 
     @Test
-    @Transactional
     public void saveAndFind() throws Exception{
         //given
         User user1 = user1();
@@ -61,5 +68,27 @@ class OrderRepositoryTest {
         Order savedOrder = orderRepository.findById(id).get();
         assertEquals(user2,savedOrder.getBuyer());
 
+    }
+
+    @Test
+    @Rollback(value = false)
+    public void findBySeller() throws Exception{
+        //given
+        User user = user1();
+        UsedBook book1 = book(user);
+        UsedBook book2 = book(user);
+        userRepository.save(user);
+        usedBookRepository.save(book1);
+        usedBookRepository.save(book2);
+        User user2 = user2();
+        userRepository.save(user2);
+        //when
+        Order order1 = Order.createOrder(user2,null,book1);
+        Order order2 = Order.createOrder(user2,null,book2);
+        orderRepository.save(order1);
+        orderRepository.save(order2);
+        //then
+        List<Order> bySeller = orderRepository.findBySeller(user.getId());
+        assertEquals(2,bySeller.size());
     }
 }
--- src/test/java/com/bookpie/shop/service/OrderServiceTest.java
+++ src/test/java/com/bookpie/shop/service/OrderServiceTest.java
@@ -5,6 +5,8 @@ import com.bookpie.shop.domain.Point;
 import com.bookpie.shop.domain.UsedBook;
 import com.bookpie.shop.domain.User;
 import com.bookpie.shop.domain.dto.OrderCreateDto;
+import com.bookpie.shop.domain.dto.OrderDto;
+import com.bookpie.shop.domain.enums.BookState;
 import com.bookpie.shop.repository.OrderRepository;
 import com.bookpie.shop.repository.UsedBookRepository;
 import com.bookpie.shop.repository.UserRepository;
@@ -26,27 +28,40 @@ class OrderServiceTest {
     @Autowired UserRepository userRepository;
     @Autowired UsedBookRepository usedBookRepository;
     @Autowired OrderRepository orderRepository;
-    User user(){
+
+
+    User user1(){
         return User.builder()
-                        .name("kim")
-                        .email("test@gmail")
-                        .point(Point.createDefaultPoint())
-                        .build();
-
+                   .name("user1")
+                   .nickName("nick1")
+                   .point(Point.createDefaultPoint())
+                   .email("user1@gmail.com")
+                   .build();
     }
 
-    UsedBook usedBook(){
+    User user2(){
+        return User.builder()
+                   .name("user2")
+                   .nickName("nick2")
+                   .email("user2@gmail.com")
+                   .point(Point.createDefaultPoint())
+                   .build();
+    }
+
+    UsedBook book(User user){
         return UsedBook.builder()
-                .title("title")
-                .build();
+                       .price(1000)
+                       .title("책 팝니다")
+                       .seller(user)
+                       .bookState(BookState.UNRELEASED)
+                       .build();
     }
-
     @Test
     public void orderSaveTest() throws Exception{
         //given
-        User user = user();
+        User user = user1();
         userRepository.save(user);
-        UsedBook usedBook = usedBook();
+        UsedBook usedBook = book(user);
         usedBookRepository.save(usedBook);
         //when
         OrderCreateDto dto = new OrderCreateDto();
@@ -55,7 +70,25 @@ class OrderServiceTest {
         //then
         Long id = orderService.saveOrder(dto);
         Order saveOrder = orderRepository.findDetailById(id).get();
-        assertEquals("kim",saveOrder.getBuyer().getName());
-        assertEquals("title",saveOrder.getBook().getTitle());
+        assertEquals("user1",saveOrder.getBuyer().getName());
+        assertEquals("책 팝니다",saveOrder.getBook().getTitle());
+    }
+
+    @Test
+    public void findByIdTest() throws Exception{
+        //given
+        User user1 = user1();
+        User user2 = user2();
+        UsedBook book = book(user1);
+        usedBookRepository.save(book);
+        userRepository.save(user1);
+        userRepository.save(user2);
+        Order order = Order.createOrder(user2,null,book);
+        Long id = orderRepository.save(order);
+        //when
+        OrderDto dto = orderService.getOrderDetail(id);
+        //then
+        assertEquals(user2.getNickName(),dto.getBuyer().getNickName());
+        assertEquals(user1.getNickName(),dto.getSeller().getNickName());
     }
 }
