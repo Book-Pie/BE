95cbc679e8720dba508baf43381d7364dec81cf4
start-27 <minw7092@naver.com>
2022-01-12 16:24:45 +0900

    refactor : 코드 수정

--- src/main/java/com/bookpie/shop/service/ReviewLikeService.java
+++ src/main/java/com/bookpie/shop/service/ReviewLikeService.java
@@ -3,6 +3,7 @@ package com.bookpie.shop.service;
 import com.bookpie.shop.domain.BookReview;
 import com.bookpie.shop.domain.ReviewLike;
 import com.bookpie.shop.domain.User;
+import com.bookpie.shop.domain.dto.book_review.BookReviewDto;
 import com.bookpie.shop.repository.BookReviewRepository;
 import com.bookpie.shop.repository.ReviewLikeRepository;
 import com.bookpie.shop.repository.UserRepository;
@@ -11,7 +12,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 @Slf4j
@@ -23,25 +23,25 @@ public class ReviewLikeService {
     @Autowired
     private UserRepository userRepository;
 
-    public String like(Long review_id) {
+    public String like(BookReviewDto dto) {
         // 해당 리뷰가 있는지 확인
-        BookReview bookReview = bookReviewRepository.findById(review_id)
+        BookReview bookReview = bookReviewRepository.findById(dto.getReviewId())
                 .orElseThrow(() -> new IllegalArgumentException("해당 리뷰는 존재하지 않습니다."));
-        // 해당 유저 확인 (실제로 user_id는 세션값에서 가져옴)
-        Optional<User> user = userRepository.findById(1L);
-        User objUser = user.orElse(null);
+
+        User user = userRepository.findById(dto.getUserId())
+                .orElseThrow(() -> new IllegalArgumentException("해당 유저는 존재하지 않습니다."));
 
         // 해당 회원이 좋아요를 눌렀는지 안눌렀는지 확인
         List<ReviewLike> reviewLikeList = bookReview.getReviewLikes();
         for (ReviewLike reviewLike : reviewLikeList) {
-            if (reviewLike.getUser().getId() == objUser.getId()) {
+            if (reviewLike.getUser().getId() == user.getId()) {
                 reviewLikeRepository.delete(reviewLike);
                 return "좋아요 삭제";
             }
         }
 
         // 연관 관계 매핑
-        ReviewLike reviewLike = ReviewLike.createReviewLike(bookReview, objUser);
+        ReviewLike reviewLike = ReviewLike.createReviewLike(bookReview, user);
         bookReview.getReviewLikes().add(reviewLike);
 
         ReviewLike created = reviewLikeRepository.save(reviewLike);
