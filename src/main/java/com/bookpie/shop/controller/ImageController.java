4762eba2e42146d1c116329d728424bffcf19066
kky0426 <ksakdlf99@naver.com>
2022-01-13 22:11:47 +0900

    Build : 이미지 캐시 설정

--- src/main/java/com/bookpie/shop/controller/ImageController.java
+++ src/main/java/com/bookpie/shop/controller/ImageController.java
@@ -6,10 +6,7 @@ import org.apache.tomcat.util.http.fileupload.FileUploadException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.Resource;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +16,7 @@ import java.io.FileNotFoundException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.concurrent.TimeUnit;
 
 import static com.bookpie.shop.utils.ApiUtil.*;
 
@@ -40,6 +38,7 @@ public class ImageController {
 
         Path path = null;
         HttpHeaders header = new HttpHeaders();
+        header.setCacheControl(CacheControl.maxAge(120, TimeUnit.SECONDS));
         try{
             path = Paths.get(filePath+name);
             header.add("Content-Type", Files.probeContentType(path));
