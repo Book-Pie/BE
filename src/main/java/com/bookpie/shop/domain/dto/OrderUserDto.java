5db7343942f45aa13728bb5611f0f5abd02f05cd
kky0426 <ksakdlf99@naver.com>
2022-01-18 18:30:43 +0900

    Feat : 주문 상세 휴대폰번호,이름 추가

--- src/main/java/com/bookpie/shop/domain/dto/OrderUserDto.java
+++ src/main/java/com/bookpie/shop/domain/dto/OrderUserDto.java
@@ -11,18 +11,23 @@ import lombok.Data;
 public class OrderUserDto {
     private Long userId;
     private String nickName;
+    private String name;
     private Address address;
-
+    private String phone;
     @Builder
     public OrderUserDto(User user, Address address){
         this.userId = user.getId();
         this.nickName = user.getNickName();
         this.address = address;
+        this.phone = user.getPhone();
+        this.name = user.getName();
     }
 
     public OrderUserDto(User user){
         this.userId = user.getId();
         this.nickName = user.getNickName();
         this.address = user.getAddress();
+        this.phone = user.getPhone();
+        this.name = user.getName();
     }
 }
