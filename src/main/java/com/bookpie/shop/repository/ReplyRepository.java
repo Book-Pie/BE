6cc4add884dca7b72a2c80ff87d0b0a658b51651
start-27 <minw7092@naver.com>
2022-01-22 21:59:36 +0900

    Chore : 게시글, 중고도서 댓글 조회 countQuery 수정

--- src/main/java/com/bookpie/shop/repository/ReplyRepository.java
+++ src/main/java/com/bookpie/shop/repository/ReplyRepository.java
@@ -14,13 +14,13 @@ public interface ReplyRepository extends JpaRepository<Reply, Long> {
     @Query(value = "select reply_id, content, reply_date, board_id," +
             " coalesce(parent_reply_id, 0) as parent_reply_id, coalesce(used_id, 0) as used_id, user_id, secret" +
             " from bookpie.reply where board_id=:board_id order by reply_date desc",
-            countQuery = "select count(*) from bookpie.reply", nativeQuery = true)
+            countQuery = "select count(*) from bookpie.reply where board_id=:board_id", nativeQuery = true)
     Page<Reply> findAllByBoard(@Param("board_id") Long board_id, @Param("pageable") Pageable pageable);
 
     // 중고도서 댓글 조회
     @Query(value = "select r.reply_id, r.content, r.reply_date, r.used_id," +
             " coalesce(r.parent_reply_id, 0) as parent_reply_id, coalesce(r.board_id, 0) as board_id, r.user_id, r.secret" +
             " from reply r where r.used_id = :used_id"
-            , countQuery = "select count(*) from bookpie.reply", nativeQuery = true)
+            , countQuery = "select count(*) from bookpie.reply where used_id=:used_id", nativeQuery = true)
     Page<Reply> findAllByUsedBook(@Param("used_id") Long used_id, @Param("pageable") Pageable pageable);
 }
