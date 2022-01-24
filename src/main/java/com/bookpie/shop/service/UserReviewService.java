317561e36c5cbbe112a0d7000f06024810356cfc
kky0426 <ksakdlf99@naver.com>
2022-01-24 15:35:38 +0900

    Feat : 회원리뷰 삭제시 reviewId  반환하도록 수정

--- src/main/java/com/bookpie/shop/service/UserReviewService.java
+++ src/main/java/com/bookpie/shop/service/UserReviewService.java
@@ -38,14 +38,15 @@ public class UserReviewService {
     }
 
     @Transactional
-    public boolean deleteUserReview(Long reviewId,Long userId){
+    public Long deleteUserReview(Long reviewId,Long userId){
         UserReview userReview = userReviewRepository.findById(reviewId)
                                                     .orElseThrow(()->new EntityNotFoundException("등록된 리뷰가 없습니다."));
         if(userReview.getOrder().getBuyer().getId() != userId){
             throw new IllegalArgumentException("리뷰 등록자가 아닙니다.");
         }
         userReview.getOrder().removeReview();
-        return userReviewRepository.remove(userReview);
+        userReviewRepository.remove(userReview);
+        return reviewId;
     }
 
     @Transactional
