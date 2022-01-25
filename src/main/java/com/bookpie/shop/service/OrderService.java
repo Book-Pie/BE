a12b104d97500f175feab405652ef297f910fdbc
kky0426 <ksakdlf99@naver.com>
2022-01-25 19:09:18 +0900

    Chore : log 수정

--- src/main/java/com/bookpie/shop/service/OrderService.java
+++ src/main/java/com/bookpie/shop/service/OrderService.java
@@ -102,6 +102,7 @@ public class OrderService {
 
     @Transactional
     public boolean orderEnd(Long orderId,Long userId){
+        log.info(orderId.toString(),userId.toString());
         Order order = orderRepository.findDetailById(orderId).orElseThrow(()->new EntityNotFoundException("주문을 찾을 수 없습니다."));
         if (order.getBuyer().getId() != userId) throw new IllegalArgumentException("구매자가 아닙니다.");
         order.end();
