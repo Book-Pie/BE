faf2f43f34d30cbd541ed9384285c34c1c294eed
start-27 <minw7092@naver.com>
2022-01-31 15:35:38 +0900

    Feat : 좋아요 눌렀을 때 book_review 테이블의 like_cnt 컬럼도 증가하거나 감소하는 기능 추가

--- src/main/java/com/bookpie/shop/service/ReviewLikeService.java
+++ src/main/java/com/bookpie/shop/service/ReviewLikeService.java
@@ -31,18 +31,20 @@ public class ReviewLikeService {
         User user = userRepository.findById(dto.getUserId())
                 .orElseThrow(() -> new IllegalArgumentException("해당 유저는 존재하지 않습니다."));
 
-        // 해당 회원이 좋아요를 눌렀는지 안눌렀는지 확인
+        // 해당 회원이 좋아요를 눌렀으면 취소하는 메서드
         List<ReviewLike> reviewLikes = reviewLikeRepository.findAllByReviewId(dto.getReviewId());
         for (ReviewLike reviewLike : reviewLikes) {
             if (reviewLike.getUser().getId() == user.getId()) {
                 reviewLikeRepository.delete(reviewLike);
+                bookReviewRepository.decreaseLikeCnt(dto.getReviewId());
                 return ReviewLikeDto.createDto(reviewLike, false);
             }
         }
 
-        // 연관 관계 매핑
+        // 좋아요를 안눌렀으면 좋아요 추가
         ReviewLike reviewLike = ReviewLike.createReviewLike(bookReview, user);
         bookReview.getReviewLikes().add(reviewLike);
+        bookReviewRepository.increaseLikeCnt(dto.getReviewId());
 
         ReviewLike created = reviewLikeRepository.save(reviewLike);
         return ReviewLikeDto.createDto(created, true);
