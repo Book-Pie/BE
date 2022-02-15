552ce60c8a92b02db3112cc17e31c2f7b8831be0
start-27 <minw7092@naver.com>
2022-02-15 12:08:30 +0900

    Chore : 쿠키로 조회수 증가 막는 부분 삭제 (현재 작동 안됨)

--- src/main/java/com/bookpie/shop/controller/BoardController.java
+++ src/main/java/com/bookpie/shop/controller/BoardController.java
@@ -11,11 +11,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
-
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import static com.bookpie.shop.utils.ApiUtil.success;
 
 @RestController
@@ -53,37 +48,8 @@ public class BoardController {
 
     // 게시글 상세 조회
     @GetMapping("/{boardId}")
-    public ResponseEntity get(@PathVariable Long boardId,HttpServletRequest req,HttpServletResponse rep) {
-        Cookie oldCookie = null;
-        Cookie cookies[] = req.getCookies();
-
-        if (cookies != null) {
-            log.info("쿠키가 존재합니다.");
-            for (Cookie  cookie : cookies) {
-                if (cookie.getName().equals("postView")) {
-                    oldCookie = cookie;
-                }
-            }
-        }
-        if (oldCookie != null) {
-            log.info("oldCookie가 존재합니다.");
-            //oldCookie가 존재하지만 해당 번호를 가지지 않았을 경우 조회수 +1
-            if (!oldCookie.getValue().contains("[" + boardId + "]")) {
-                boardService.viewPlus(boardId);
-                oldCookie.setValue(oldCookie.getValue()+"_["+boardId+"]");
-                oldCookie.setPath("/");
-                oldCookie.setMaxAge(60 * 60 * 24);
-                rep.addCookie(oldCookie);
-            }
-        }else {
-            // oldCookie 존재하지 않을 경우 쿠키 새로 만들고 조회수 + 1
-            log.info("oldCookie가 존재하지 않습니다.");
-            Cookie newCookie = new Cookie("postView", "["+boardId+"}");
-            boardService.viewPlus(boardId);
-            newCookie.setPath("/");
-            newCookie.setMaxAge(60 * 60 * 24);
-            rep.addCookie(newCookie);
-        }
+    public ResponseEntity get(@PathVariable Long boardId) {
+        boardService.viewPlus(boardId);
         return new ResponseEntity(success(boardService.getBoard(boardId)), HttpStatus.OK);
     }
 
