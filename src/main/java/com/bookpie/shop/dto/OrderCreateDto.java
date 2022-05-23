842cc9c97a009a369b8ff01a77d88d9ecfe46991
kky0426 <ksakdlf99@naver.com>
2022-05-23 19:24:41 +0900

    Chore : 빌더, 생성자 관련 어노테이션 추가

--- src/main/java/com/bookpie/shop/dto/OrderCreateDto.java
+++ src/main/java/com/bookpie/shop/dto/OrderCreateDto.java
@@ -1,9 +1,15 @@
 package com.bookpie.shop.dto;
 
 import com.bookpie.shop.domain.Address;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class OrderCreateDto {
     private Long userId;
     private Long usedBookId;
